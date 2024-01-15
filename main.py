287b68f7184d3b496593ff593fb0595cc97d505e
Viphava280444 <87804447+Viphava280444@users.noreply.github.com>
2024-01-15 10:45:31 +0700

    create link to source code

--- main.py
+++ main.py
@@ -26,17 +26,21 @@ Below you can find some of the apps I have built in Python. Feel free to contact
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col,col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df.iterrows():
         if index % 2 == 0:
             st.header(row['title'])
+            st.write(row['description'])
             st.image(f'images/{row["image"]}')
+            st.write('[Source Code](https://google.com)')
 
 with col4:
     for index, row in df.iterrows():
         if index % 2 != 0:
             st.header(row['title'])
+            st.write(row['description'])
             st.image(f'images/{row["image"]}')
+            st.write('[Source Code](https://google.com)')
